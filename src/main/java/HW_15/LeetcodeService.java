00afc1e851473892aa25099876413f93c4b7b37f
ovoskresenskyy <7949894@ukr.net>
2022-11-08 23:15:13 +0200

    Rename main

--- src/main/java/HW_15/LeetcodeService.java
+++ src/main/java/HW_15/LeetcodeService.java
@@ -3,15 +3,16 @@ package HW_15;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Main {
+public class LeetcodeService {
 
-    public static int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> integers = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
             int difference = target - nums[i];
             if (integers.containsKey(difference)) return new int[] { integers.get(difference), i };
             else integers.put(nums[i], i);
         }
-        return null;
+        return new int[2];
     }
 }
