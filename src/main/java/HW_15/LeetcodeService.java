1f2a23e0ae275fc99e6bd80d51d88c013de5c108
ovoskresenskyy <7949894@ukr.net>
2022-11-10 14:48:52 +0200

    Added solution for twoSum

--- src/main/java/HW_15/LeetcodeService.java
+++ src/main/java/HW_15/LeetcodeService.java
@@ -1,4 +1,18 @@
 package HW_15;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LeetcodeService {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> integers = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int difference = target - nums[i];
+            if (integers.containsKey(difference)) return new int[]{integers.get(difference), i};
+            else integers.put(nums[i], i);
+        }
+        return new int[2];
+    }
 }
