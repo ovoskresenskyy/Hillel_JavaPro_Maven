9deb990ba0bdaddcaceaa6c7ad0283388e23b18e
ovoskresenskyy <7949894@ukr.net>
2022-11-10 14:31:35 +0200

    Add tests for Fibonacci sequence and generating Pascal's triangle

--- src/test/java/HW_15/LeetcodeServiceTest.java
+++ src/test/java/HW_15/LeetcodeServiceTest.java
@@ -4,6 +4,10 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class LeetcodeServiceTest {
 
     public LeetcodeService leetcodeService;
@@ -31,5 +35,39 @@ public class LeetcodeServiceTest {
         Assert.assertArrayEquals(result, leetcodeService.twoSum(nums, target));
     }
 
+    @Test
+    public void formFibonacciSequence_ShouldReturnCorrectResult() {
+        int input = 4;
+        int result = 3;
 
+        Assert.assertEquals(result, leetcodeService.formFibonacciSequence(input));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void formFibonacciSequence_ShouldThrowArithmeticException() {
+        leetcodeService.formFibonacciSequence(-1);
+    }
+
+    @Test
+    public void generatePascalsTriangle_ShouldReturnCorrectResult() {
+        int numRows = 5;
+        List<List<Integer>> expectedResult = new ArrayList<>();
+        expectedResult.add(new ArrayList<>(List.of(1)));
+        expectedResult.add(new ArrayList<>(List.of(1, 1)));
+        expectedResult.add(new ArrayList<>(List.of(1, 2, 1)));
+        expectedResult.add(new ArrayList<>(List.of(1, 3, 3, 1)));
+        expectedResult.add(new ArrayList<>(List.of(1, 4, 6, 4, 1)));
+
+        Assert.assertEquals(expectedResult, leetcodeService.generatePascalsTriangle(numRows));
+    }
+
+    @Test
+    public void generatePascalsTriangle_ShouldReturnEmptyArrayList() {
+        Assert.assertEquals(new ArrayList<>(), leetcodeService.generatePascalsTriangle(0));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void generatePascalsTriangle_ShouldThrowArithmeticException() {
+        leetcodeService.generatePascalsTriangle(-1);
+    }
 }
