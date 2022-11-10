e4073492d58f02a1758361955acd9bff483910ef
ovoskresenskyy <7949894@ukr.net>
2022-11-10 14:50:16 +0200

    Added tests for twoSum

---
+++ src/test/java/HW_15/LeetcodeServiceTest.java
@@ -0,0 +1,33 @@
+package HW_15;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LeetcodeServiceTest {
+
+    public LeetcodeService leetcodeService;
+
+    @Before
+    public void init() {
+        leetcodeService = new LeetcodeService();
+    }
+
+    @Test
+    public void twoSum_ShouldReturnCorrectResult() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = {0, 1};
+
+        Assert.assertArrayEquals(result, leetcodeService.twoSum(nums, target));
+    }
+
+    @Test
+    public void twoSum_ShouldReturnEmptyArray() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 99;
+        int[] result = {0, 0};
+
+        Assert.assertArrayEquals(result, leetcodeService.twoSum(nums, target));
+    }
+}
