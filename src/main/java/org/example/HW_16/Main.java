d064e262a7c698ba6841b527eabfdfce0d563c40
ovoskresenskyy <7949894@ukr.net>
2022-11-13 22:37:27 +0200

    added start point

--- src/main/java/org/example/HW_16/Main.java
+++ src/main/java/org/example/HW_16/Main.java
@@ -1,7 +1,15 @@
 package org.example.HW_16;
 
+import org.example.HW_16.services.PetShelterService;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        PetShelterService petShelterService = new PetShelterService(scanner);
+        petShelterService.start();
+        scanner.close();
     }
 }
