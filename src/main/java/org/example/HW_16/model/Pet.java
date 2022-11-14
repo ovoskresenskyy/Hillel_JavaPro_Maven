154b3d77a96e67e5a2a5c60d7243a5b6959d9902
ovoskresenskyy <7949894@ukr.net>
2022-11-14 22:25:03 +0200

    Added @Jaksonized and @Data to correct serializing

--- src/main/java/org/example/HW_16/model/Pet.java
+++ src/main/java/org/example/HW_16/model/Pet.java
@@ -1,19 +1,19 @@
 package org.example.HW_16.model;
 
 import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 import org.example.HW_16.enums.Gender;
 import org.example.HW_16.enums.KindOfPet;
 
 @Builder
+@Data
+@Jacksonized
 public class Pet {
 
     private final KindOfPet kind;
     private final String breed;
     private final Gender gender;
-
-    public KindOfPet getKind() {
-        return kind;
-    }
 
     @Override
     public String toString() {
